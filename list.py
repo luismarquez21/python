f5d9707071331e3a0f1f06c803825877afaf934e
Luis Marquez <luismx@DESKTOP-3RSA8QA.localdomain>
2022-03-21 13:50:25 -0600

    mi primer commit

---
+++ list.py
@@ -0,0 +1,21 @@
+#/usr/bin/python3
+
+###############################################################################
+#                                                                             #
+# Este programa imprime el mensaje "Hola, Mundo"                              #
+#                                                                             #
+# Ultima modificacion: 21 de Marzo de 2022                                  #
+###############################################################################
+
+print ('Hola, Mundo') 
+
+#FIN DEL SCRIPT
+
+
+
+
+
+
+
+
+
